c8309735b34d1f1a99875aefe04a717b0041562a
DutchAssassin <42503862+DutchAssassin@users.noreply.github.com>
2019-10-02 01:01:42 +0200

    Add moderation commands
    Add some commonly used moderation commands

--- main.py
+++ main.py
@@ -10,15 +10,26 @@ async def on_ready():
 @bot.command()
 async def brexit(ctx):
     await ctx.send("I agree")
+   
+@bot.command()
+async def kick(ctx, member:discord.Member):
+    await ctx.guild.kick(member, reason="kicked!")
+    await ctx.send(f"{member.name} has been kicked from the server!")
+
+@bot.command()
+async def ban(ctx, member:discord.Member):
+    await ctx.guild.ban(member, reason="banned!")
+    await ctx.send(f"{member.name} has been banned from the server!")
     
-@bot.group()
-async def define(ctx):
-    await ctx.send("Please choose something to define.")
+@bot.command()
+async def nick(ctx, member:discord.Member, *, nick:str):
+    await member.edit(nick=nick)
+    await ctx.send(f"Changed {member.name}'s nickname to {nick}")
     
-@define.command()
-async def boris_johnson(ctx):
-    await ctx.send("A Poundland version of Donald Trump." \
-                   "e.g If someone is seen to be copying someone else but in a worse manner," \
-                   "you could say they are an absolute boris johnson")
+@bot.command()
+async def hackban(ctx, member_id:int):
+    member = discord.Object(id=member_id)
+    await ctx.guild.ban(member, reason="hackbanned!")
+    await ctx.send(f"User with ID: {str(member_id)} has been banned from the server!")
 
 bot.run("token")
